05d3d16b207a4f70a0012471b0635f71f6d124ee
code <code@maccui-MacBook-Air.local>
2020-08-15 22:57:54 -0400

    refining the logic for adding the item to basket

--- src/reducer.js
+++ src/reducer.js
@@ -22,7 +22,11 @@ const reducer = (state, action) => {
 	// The constant reducer stores a function that has the two parameter state and action.
 	// We refer to the data layer as state; so anything inside the data layer is known as state.
 	// We manipulate the data layer with actions; such as: add basket, add item to basket, remove item from basket,etc
+
 	console.log(action);
+	// console log the items that are clicked on
+	// this is to check that the click function is working
+
 	switch (action.type) {
 		// switch stores an action, and the 'type' of that action
 		// switch also checks all the cases (strings that represent a specific action )
@@ -30,7 +34,21 @@ const reducer = (state, action) => {
 		case "ADD_TO_BASKET":
 			// this first case "ADD_TO_BASKET" represent the action of adding an item to the basket
 			// Logic for adding item to basket
-			return { state };
+
+			return {
+				// a new data layer that is compased of
+
+				...state, // Return whatever the state current way
+				// If you had other properties, then this is where you insert all your properties
+
+				// Similar to how you declare state in the constructor
+				// and then set.state in componentDidMount
+				basket: [...state.basket, action.item]
+				// Here you are creating a new basket that's going to contain:
+				// The current basket (whatever items were left over) + action.item (the item that was just picked)
+
+				// (action.item gets the data points that we asked from in the addtobasket function )
+			};
 		// break; // this break signifies that the case logic is finished
 
 		case "REMOVE_FROM_BASKET":
