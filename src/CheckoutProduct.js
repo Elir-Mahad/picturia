3b055783be799cfc46c219aa4f6fb854a7e312e5
code <mmahad280@gmail.com>
2020-08-16 21:30:01 -0400

    styling for check out

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -5,7 +5,7 @@ import "./checkoutProduct.css";
 function CheckoutProduct({ id, title, image, price, rating }) {
 	return (
 		<div className="checkoutProduct">
-			<img src={image} alt="" />
+			<img className="checkoutProduct_image" src={image} alt="" />
 			<div className="checkoutProduct_info">
 				<p className="checkoutProduct_title">{title}</p>
 				<p className="checkoutProduct_price">
@@ -21,7 +21,7 @@ function CheckoutProduct({ id, title, image, price, rating }) {
 							<p>⭐</p>
 						))}
 				</div>
-				{/* <button onClick={RemoveFromBasket}>Add to basket</button> */}
+				<button>Add to basket</button>
 			</div>
 		</div>
 	);
